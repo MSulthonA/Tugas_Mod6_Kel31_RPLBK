cb1a8d91af19f9d595fad11eb6a709796a7511bc
MSulthonA <muhammadsulthonauliya313@gmail.com>
2023-11-12 05:05:01 +0700

    first commit

---
+++ src/main/java/tracker/ejb/TrackerBeanLocal.java
@@ -0,0 +1,13 @@
+package tracker.ejb;
+
+import jakarta.ejb.Local;
+
+@Local
+public interface TrackerBeanLocal {
+    public double add(double value);
+    public double average();
+    public int getCount();
+    public double getTotal();
+
+}
+
